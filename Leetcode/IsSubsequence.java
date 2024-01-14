506fdd8b80b65453dafb9155fe290bee1bb2004a
Uday Kumar Maurya <122080383+udaykumar2507@users.noreply.github.com>
2024-01-14 23:49:46 +0530

    Create IsSubsequence.java

---
+++ Leetcode/IsSubsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        int len1=s.length();
+        int len2=t.length();
+        while(i<len1 && j<len2){
+            if (s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        System.out.print(i);
+        if (i==len1){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
